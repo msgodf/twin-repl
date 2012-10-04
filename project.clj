d28e0e822ee3008ee850c8c70dc78fe444344ba1
Mark Godfrey <msgodf@gmail.com>
2012-10-04 17:40:08 +0100

    Fix formatting.
    I was using vanilla Vim *blush* to do the initial project setup.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject twin-repl "0.1.0-SNAPSHOT"
-            :description "A Clojure and ClojureScript project to demonstrate using REPLs to modify the live client and server parts of a webapp."
-            :url "http://github.com/msgodf/twin-repl"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [hiccup "1.0.0"]
-                           [compojure "1.1.1"]
-                           [ring/ring-jetty-adapter "1.1.0"]]
-            :plugins [[lein-ring "0.7.0"]]
-            :ring {:handler twin-repl.core/app}
-            :main twin-repl.core)
+  :description "A Clojure and ClojureScript project to demonstrate using REPLs to modify the live client and server parts of a webapp."
+  :url "http://github.com/msgodf/twin-repl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.0"]
+                 [compojure "1.1.1"]
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]]
+  :plugins [[lein-ring "0.7.0"]]
+  :ring {:handler twin-repl.core/app}
+  :main twin-repl.core)
--- src/twin_repl/core.clj
+++ src/twin_repl/core.clj
@@ -8,14 +8,14 @@
 ; a little bit of HTML as an index page, using hiccup for cleanliness
 (defn index-page []
   (html5
-    [:head
-     [:title "twin-repl"]]
-    [:body
-     [:p "Hello REPL"]]))
+   [:head
+    [:title "twin-repl"]]
+   [:body
+    [:p "Hello REPL"]]))
 
 (compojure/defroutes routes
-                     (compojure/GET "/" [] (index-page))
-                     (route/resources "/"))
+  (compojure/GET "/" [] (index-page))
+  (route/resources "/"))
 
 (def app (handler/api routes))
 
